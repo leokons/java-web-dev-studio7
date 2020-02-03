32bf804adffe190ef728aa3680ca302eacacf67f
Konstantin <phoenix05@gmail.com>
2020-02-03 00:11:40 -0600

    Studio 8.7

--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -8,5 +8,20 @@ public class Main {
 
         // TODO: Call each CD and DVD method to verify that they work as expected.
 
+        CD cd = new CD("CD example", 700, "CD-R", 350);
+        DVD dvd = new DVD("DVD example", 4700, "DVD-R", 1450);
+
+        cd.spinDisc();
+        dvd.spinDisc();
+
+        cd.readData();
+        dvd.readData();
+
+        System.out.println(cd.writeData(275));
+        System.out.println(dvd.writeData(8000));
+
+        System.out.println(cd.diskInfo());
+        System.out.println(dvd.diskInfo());
+
     }
 }
